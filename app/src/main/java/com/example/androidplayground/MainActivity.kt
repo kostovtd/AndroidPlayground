db89b7b44540ae1dd5f08250f6fbb0600bdac8ee
kostovtd <kostovtd@gmail.com>
2022-09-20 10:03:56 +0300

    Simple Compose functionality included

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.androidplayground
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Space
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(message = Message(
+                        "Todor",
+                        "Hello! How are you? 2"
+                    ))
+                }
+            }
+        }
+    }
+
+
+
+    data class Message(val author: String, val body: String)
+
+    @Composable
+    fun MessageCard(message: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.error, CircleShape)
+            )
+            
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.error,
+                    style = MaterialTheme.typography.subtitle2
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+        }
+    }
+
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        name = "Dark Mode",
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        MaterialTheme {
+            Surface {
+                MessageCard(
+                    message = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+                )
+            }
+        }
     }
 }
